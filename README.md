13817000b1b99402984b7f106b77a941b8b50fed
Romain Baugue <romain.baugue@elwinar.com>
2020-09-18 23:42:23 +0200

    Read the configuration file

--- README.md
+++
@@ -1,2 +0,0 @@
-# b16m
-base16-manager type that don't suck
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use serde::Deserialize;
+use std::fs::File;
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(default)]
+struct Config {
+    scheme: String,
+    scheme_repository_url: String,
+    schemes_list_url: String,
+    templates_list_url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = match File::open("/home/elwinar/.config/b16m/config.yaml") {
+        Ok(res) => res,
+        Err(err) => panic!("opening configuration file: {:?}", err),
+    };
+    let config: Config = match serde_yaml::from_reader(&config) {
+        Ok(res) => res,
+        Err(err) => panic!("parsing configuration file: {:?}", err),
+    };
+
+    println!("config: {:?}", config);
 }
