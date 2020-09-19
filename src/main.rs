c660f8b696a31e609b962db2d5007c547fabbf1e
Romain Baugue <romain.baugue@elwinar.com>
2020-09-19 09:03:36 +0200

    Complete the configuration file structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
-use serde::Deserialize;
-use std::fs::File;
+use serde;
+use std::collections;
+use std::fs;
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, serde::Deserialize)]
 #[serde(default)]
 struct Config {
     scheme: String,
     scheme_repository_url: String,
     schemes_list_url: String,
     templates_list_url: String,
+    applications: collections::HashMap<String, Application>,
+}
+
+#[derive(Debug, Default, serde::Deserialize)]
+#[serde(default)]
+struct Application {
+    hook: String,
+    template_repository_url: String,
+    files: collections::HashMap<String, File>,
+}
+
+#[derive(Debug, Default, serde::Deserialize)]
+#[serde(default)]
+struct File {
+    destination: String,
+    mode: String,
+    start_marker: String,
+    end_marker: String,
 }
 
 fn main() {
-    let config = match File::open("/home/elwinar/.config/b16m/config.yaml") {
+    let config = match fs::File::open("/home/elwinar/.config/b16m/config.yaml") {
         Ok(res) => res,
         Err(err) => panic!("opening configuration file: {:?}", err),
     };
