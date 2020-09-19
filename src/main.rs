856cc9ba30ca41f2ac358b8c3da194aeeef527c9
Romain Baugue <romain.baugue@elwinar.com>
2020-09-19 11:36:39 +0200

    Retrieve the scheme list

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use reqwest;
 use serde;
 use std::collections;
+use std::env;
 use std::fs;
+use std::io::Read;
 
 #[derive(Debug, Default, serde::Deserialize)]
 #[serde(default)]
 struct Config {
     scheme: String,
     scheme_repository_url: String,
+    #[serde(default = "default_scheme_list_url")]
     schemes_list_url: String,
+    #[serde(default = "default_templates_list_url")]
     templates_list_url: String,
     applications: collections::HashMap<String, Application>,
 }
@@ -34,10 +39,55 @@ fn main() {
         Ok(res) => res,
         Err(err) => panic!("opening configuration file: {:?}", err),
     };
-    let config: Config = match serde_yaml::from_reader(&config) {
+    let mut config: Config = match serde_yaml::from_reader(&config) {
         Ok(res) => res,
         Err(err) => panic!("parsing configuration file: {:?}", err),
     };
 
-    println!("config: {:?}", config);
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => {}
+        2 => {
+            config.scheme = args[1].clone();
+        }
+        3 => {
+            config.scheme = args[1].clone();
+            config.scheme_repository_url = args[2].clone();
+        }
+        _ => panic!("too many arguments"),
+    };
+
+    let client = reqwest::blocking::Client::new();
+
+    let mut res = match client.get(&config.schemes_list_url).send() {
+        Ok(res) => res,
+        Err(err) => panic!("retrieving schemes list: {:?}", err),
+    };
+
+    let mut body = String::new();
+    match res.read_to_string(&mut body) {
+        Err(err) => panic!("reading response body: {:?}", err),
+        _ => {}
+    }
+
+    if !res.status().is_success() {
+        panic!("unexpected status: {:?} {}", res.status(), body);
+    }
+
+    println!("{}", body);
+}
+
+fn default_scheme_list_url() -> String {
+    github_file_url("chriskempson", "base16-schemes-source", "list.yaml")
+}
+
+fn default_templates_list_url() -> String {
+    github_file_url("chriskempson", "base16-templates-source", "list.yaml")
+}
+
+fn github_file_url(user: &str, repository: &str, url: &str) -> String {
+    format!(
+        "https://raw.githubusercontent.com/{}/{}/master/{}",
+        user, repository, url
+    )
 }
